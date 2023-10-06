73243a2f3ed79c90a351bd4526506dd6cd3ae7c6
Sireesha <agurusireeshaedu@gmail.com>
2023-10-06 11:51:03 +0530

    new

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 // Landing Page
 
-const sentences = ['💪 I am an Aspiring Techie 🚴🏻‍♀️','🤞🏼 Frontend Developer 💞','💖 Loves python 💘','🌟 AI/ML Enthusiast 😍'];
-const sentenceContainer = document.querySelector(".sentence-container");
-let sentenceIndex = 0;
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-function displayNextSentence() {
-    sentenceContainer.textContent = sentences[sentenceIndex];
-    sentenceContainer.classList.add('portfolio-paragraph','paragraph_styling');
-    sentenceIndex = (sentenceIndex + 1) % sentences.length;
-}
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-displayNextSentence();
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-setInterval(function(){
-    displayNextSentence();
-}, 2000); 
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+
 
 const header = document.querySelector('.navbar');
 
